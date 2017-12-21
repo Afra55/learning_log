413f31839266a46c6fa530e08e325484b3e2a9fc
Afra55 <1937525930@qq.com>
2017-12-21 22:46:50 +0800

    模板网页

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 
 from . import views
 
-
 urlpatterns = [
-    path(r'', views.index, name='index'),     # '^$' 查找开头到末尾的 URL 与基础 URL(http://127.0.0.1:8000/) 匹配
+    path(r'', views.index, name='index'),
+    path(r'topics/', views.topics, name='topics'),
+    path(r'topics/(?P<topic_id>\d+)/', views.topic, name='topic')
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
 
-
 # Create your views here.
+from learning_logs.models import Topic
+
+
 def index(request):
     return render(request, 'learning_logs/index.html')
 
+
+def topics(request):
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
+
+
+def topic(request, topic_id):
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'learning_logs/topic.html', context)
