04bf909765d80a3f323d5e9f3eaa5267d2111884
Afra55 <1937525930@qq.com>
2017-12-25 21:41:54 +0800

    自定义表单

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path(r'', views.index, name='index'),
     path(r'topics/', views.topics, name='topics'),
-    path(r'topics/(?P<topic_id>\d+)/', views.topic, name='topic')
+    path(r'topics/(?P<topic_id>\d+)/', views.topic, name='topic'),
+    path(r'new_topic/', views.new_topic, name='new_topic')
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,9 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
-
 # Create your views here.
+from django.urls import reverse
+
+from learning_logs.forms import TopicForm
 from learning_logs.models import Topic
 
 
@@ -19,3 +22,17 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
+
+def new_topic(request):
+    if request.method != 'POST':  # 判断请求的方法
+        form = TopicForm()
+    else:
+        form = TopicForm(request.POST)
+
+    if form.is_valid():  # 验证表单字段是否都填写了（默认都必须填写）
+        form.save()  # 将表单数据写入数据库
+        return HttpResponseRedirect(reverse('learning_logs:topics'))  # 重定向 URL
+
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
