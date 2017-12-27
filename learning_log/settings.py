6ef6096f4c28b353fdc764c739958bf51bbe0c09
Afra55 <1937525930@qq.com>
2017-12-27 23:15:16 +0800

    配置 Heroku， 更改 错误信息展示模板

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'learning_log.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'learning_log/templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
